152dea3725166d6b576368982eaf6127dd9defeb
william <williamsebastiaoteixeira@gmail.com>
2022-02-24 23:48:20 -0300

    CRUD simples

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,67 @@
 import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 function App() {
+  const [telefone, setTelefone] = useState(''); 
+  const [listaTelefone, setListaTelefone] = useState([]); 
+  const [novoTelefone, setNovoTelefone] = useState(''); 
+
+
+  useEffect(()=>{
+    axios.get("http://localhost:3001/api/get").then((response)=>{
+      setListaTelefone(response.data); 
+    }); 
+  });
+
+  const submitForm = () =>{
+    axios.post("http://localhost:3001/api/insert", {
+      telefone: telefone
+    }); 
+  }; 
+
+  const deleteContato = (telefone) =>{
+    axios.delete(`http://localhost:3001/api/delete/${telefone}`); 
+  }; 
+
+  const updateContato = (telefone) => {
+    axios.put("http://localhost:3001/api/update", {novoTelefone: novoTelefone, telefone: telefone}); 
+    setNovoTelefone(''); 
+  }; 
+
   return (
-    <div className="App">
-      <h1> Lista Telefonica</h1>
-      
-      <div className="form">
-        <label>Telefone</label> 
-        <input type="text" name="telefone"/>
-        <button>Enviar</button> 
+    <div className="container">
+      <div className="mt-2 row">
+        <div className="input-group mt-2">
+          <input className="form-control" aria-describedby="enviar" type="text" name="telefone" onChange={(e)=>{setTelefone(e.target.value);}}/>
+          <button className="btn btn-outline-secondary" type="button" id="enviar" onClick={submitForm}>Inserir</button>
+        </div>
+      </div>
+      <div className="row">
+        {
+          listaTelefone.map((value,i)=>{
+            return (
+              <div key={i} className="col-4 mt-2">
+                <div className="card text-center">
+                  <div className='card-header'>
+                    <input className="form-control" 
+                      type="text" 
+                      defaultValue={value.telefone} 
+                      name="novoTelefone" 
+                      onChange={(e)=>{
+                        setNovoTelefone(e.target.value)
+                      }
+                    }/>
+                  </div>
+                  <div className='card-body'>
+                    <button type="button" className="btn btn-outline-primary" onClick={()=>{updateContato(value.telefone)}}>Alterar</button>
+                    <button type="button" className="btn btn-outline-danger" onClick={()=>{deleteContato(value.telefone)}}>Excluir</button>
+                  </div>  
+                </div>
+              </div>
+            );  
+          })
+        }
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,19 +1,55 @@
 const express = require('express'); 
+const bodyParser = require('body-parser'); 
 const app = express();
 const mysql = require('mysql'); 
+const cors = require('cors')
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'william',
     password: 'Senha@123',
     database: 'LISTATELEFONICA'
-})
+}); 
 
-app.get('/', (req, res)=>{
-    const sqlInsert = "INSERT INTO contatos(nome) VALUES ('mundo');"
-    db.query(sqlInsert,(err, result) => {
-        res.send('Olá Mundo');
-    });
+app.use(cors()); 
+app.use(express.json()); 
+app.use(bodyParser.urlencoded({extended:true})); 
+
+//Get
+app.get('/api/get',(req, res)=>{
+    const sqlSelect = "SELECT * FROM LISTATELEFONICA.contatos;"
+    db.query(sqlSelect,(err, result)=>{
+        if(err) console.log(err);
+        res.send(result); 
+    }); 
+}); 
+
+//Insert
+app.post('/api/insert', (req, res)=>{
+    const telefone = req.body.telefone;  
+    const sqlInsert = "INSERT INTO contatos(telefone) VALUES (?);"
+    db.query(sqlInsert, [telefone],(err, result)=>{
+        if(err) console.log(err);
+    }); 
+}); 
+
+//delete
+app.delete('/api/delete/:telefone', (req, res)=>{
+    const telefone = req.params.telefone;
+    const sqlDelete = "DELETE FROM LISTATELEFONICA.contatos WHERE telefone = ?";
+    db.query(sqlDelete, [telefone], (err, result)=>{
+        if(err) console.log(err); 
+    })
+}); 
+
+//update
+app.put('/api/update', (req, res)=>{
+    const telefone = req.body.telefone;
+    const novoTelefone = req.body.novoTelefone;
+    const sqlUpdate = "UPDATE LISTATELEFONICA.contatos SET telefone = ? WHERE telefone = ?;";
+    db.query(sqlUpdate, [novoTelefone, telefone],(err, result)=>{
+        if(err) console.log(err);
+    }); 
 }); 
 
 app.listen(3001, ()=> {
